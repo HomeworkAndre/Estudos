7e8239016f37fb37d3279c42afe0ede687785b24
André França <andrecompunifor@gmail.com>
2020-03-20 18:09:50 -0300

    Update main01.js

--- Cursos/Rocketseat/Javascript/JS assíncrono/Desafio/main01.js
+++ Cursos/Rocketseat/Javascript/JS assíncrono/Desafio/main01.js
@@ -9,4 +9,17 @@ function Verificar(){
     inputElement.value = '';
 }
 
+function checaIdade(idade) { 
+      // Retornar uma promise 
+}
+
+checaIdade(20)
+.then(function() {
+       console.log("Maior que 18");
+})
+.catch(function() {
+        console.log("Menor que 18");
+});
+ 
+
 btnVerificar.onclick = Verificar;
